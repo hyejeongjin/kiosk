b52e64eb3f90a8e3f8eef2b0fb37448d80912520
hyejeongjin <hyejeongjin.dev@gmail.com>
2025-03-11 12:23:45 +0900

    :sparkles: lv2. Main 구현 및 MenuItem 클래스 추가

---
+++ src/lv2/Main.java
@@ -0,0 +1,74 @@
+package lv2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<MenuItem> menuItems = new ArrayList<>();
+
+        menuItems.add(new MenuItem("Espresso", 2500, "고온ㆍ고압하에서 곱게 간 커피 가루에 물을 가해, 30초 이내에 내린 커피."));
+        menuItems.add(new MenuItem("Americano", 3000, "에스프레소에 물을 넣어 연하게 마시는 커피."));
+        menuItems.add(new MenuItem("Caffé Latte", 3500, "에스프레소에 따뜻한 우유를 1:2 또는 1:3 정도의 비율로 섞은 커피."));
+        menuItems.add(new MenuItem("Caramel Macchiato", 4000, "에스프레소에 캐러멜 소스와 우유를 넣고 우유 거품만 살짝 올린 커피."));
+        menuItems.add(new MenuItem("Café Mocha", 4500, "에스프레소에 우유와 초콜릿을 넣고 휘핑크림을 올린 커피."));
+
+        List<String> selectList = new ArrayList<>();
+
+        //Scanner
+        Scanner input = new Scanner(System.in);
+        while (true) {
+
+            System.out.println("[ SKY COFFEE MENU ]");
+
+            for (int i = 0; i < menuItems.size(); i++) {
+                System.out.println(i + 1 + ". " + menuItems.get(i).toString());
+            }
+
+            System.out.print("메뉴를 선택해주세요(종료 시 0 입력): ");
+
+            int select = input.nextInt();
+            switch (select) {
+                case 1:
+                    System.out.println(1+ ". " + menuItems.get(0).getCoffee() + "을 선택하셨습니다.\n");
+                    selectList.add(menuItems.get(0).getCoffee());
+                    break;
+                case 2:
+                    System.out.println(2+ ". " + menuItems.get(1).getCoffee() + "을 선택하셨습니다.\n");
+                    selectList.add(menuItems.get(1).getCoffee());
+                    break;
+                case 3:
+                    System.out.println(3+ ". " + menuItems.get(2).getCoffee() + "을 선택하셨습니다.\n");
+                    selectList.add(menuItems.get(2).getCoffee());
+                    break;
+                case 4:
+                    System.out.println(4+ ". " + menuItems.get(3).getCoffee() + "을 선택하셨습니다.\n");
+                    selectList.add(menuItems.get(3).getCoffee());
+                    break;
+                case 5:
+                    System.out.println(5+ ". " + menuItems.get(4).getCoffee() + "을 선택하셨습니다.\n");
+                    selectList.add(menuItems.get(4).getCoffee());
+                    break;
+                case 0:
+                    System.out.println("프로그램을 종료합니다.");
+                    System.out.println("선택하신 음료는 ");
+
+                    for(int j=0; j<selectList.size(); j++){
+                        for(int k=0; k< menuItems.size(); k++){
+                            if(menuItems.get(k).getCoffee().equals(selectList.get(j))){
+                                System.out.println(menuItems.get(k).toString());
+                            }
+                        }
+                    }
+                    System.out.println("입니다.");
+
+                    System.exit(0);
+                default:
+                    System.out.println("잘못된 번호를 입력하셨습니다!");
+            }
+        }
+
+    }
+}
---
+++ src/lv2/MenuItem.java
@@ -0,0 +1,50 @@
+package lv2;
+
+
+public class MenuItem {
+
+    private String coffee;
+    private int price;
+    private String info;
+
+    // 생성자
+    public MenuItem(String coffee, int price, String info){
+        this.coffee = coffee;
+        this.price = price;
+        this.info = info;
+    }
+
+    // 재정의 없이 toString()을 사용하면 값이 출력되는 게 아니라 메모리 주소가 나오게 됨.
+    // 오버라이딩을 이용해 toString() 메서드를 재정의해서 해결할 수 있음.
+    // 참고 블로그 : https://velog.io/@wnajsldkf/toString-%EB%A9%94%EC%84%9C%EB%93%9C-%EC%98%AC%EB%B0%94%EB%A5%B4%EA%B2%8C-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0
+    @Override
+    public String toString() {
+        return coffee + "   | W " + price + " | " + info;
+    }
+
+    //getter, setter
+    public void setCoffee(String coffee) {
+        this.coffee = coffee;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public String getCoffee() {
+        return coffee;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+}
