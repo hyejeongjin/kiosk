a1d2daf364926c98b4efccbff8c98e15674b0f1e
hyejeongjin <hyejeongjin.dev@gmail.com>
2025-03-12 16:57:44 +0900

    :Chore: kiosk 최종 음료 목록 출력 조건 설정, System.exit()에서 boolean 조건을 추가해 종료하는 방식으로 수정

--- src/lv3/Kiosk.java
+++ src/lv3/Kiosk.java
@@ -7,27 +7,25 @@ import java.util.Scanner;
 public class Kiosk {
 
     // 속성
-    private final List<MenuItem> menuItems = new ArrayList<>();
+    private final List<MenuItem> menuItems;
 
     // 생성자
-
-
+    public Kiosk(List<MenuItem> menuItems){
+        this.menuItems = menuItems;
+    }
 
     // 기능
     public void start() {
 
         Scanner input = new Scanner(System.in);
 
-        menuItems.add(new MenuItem("Espresso", 2500, "고온ㆍ고압하에서 곱게 간 커피 가루에 물을 가해, 30초 이내에 내린 커피."));
-        menuItems.add(new MenuItem("Americano", 3000, "에스프레소에 물을 넣어 연하게 마시는 커피."));
-        menuItems.add(new MenuItem("Caffé Latte", 3500, "에스프레소에 따뜻한 우유를 1:2 또는 1:3 정도의 비율로 섞은 커피."));
-        menuItems.add(new MenuItem("Caramel Macchiato", 4000, "에스프레소에 캐러멜 소스와 우유를 넣고 우유 거품만 살짝 올린 커피."));
-        menuItems.add(new MenuItem("Café Mocha", 4500, "에스프레소에 우유와 초콜릿을 넣고 휘핑크림을 올린 커피."));
-
         // 선택한 메뉴를 담을 list
         List<String> selectList = new ArrayList<>();
 
-        while (true) {
+        // while문 break 용도
+        boolean check = true;
+
+        while (check) {
 
             // 메뉴 출력
             System.out.println("[ SKY COFFEE MENU ]");
@@ -44,7 +42,7 @@ public class Kiosk {
             try{
                 select = input.nextInt();
             }catch(Exception e){
-                System.out.println("숫자를 넣으세요! 오류나지 마세요 제발...");
+                System.out.println("숫자를 입력해주세요!");
                 // 버퍼를 비워서 select에 들어간 값을 비워줘야함.
                 input.nextLine();
                 continue;
@@ -73,18 +71,22 @@ public class Kiosk {
                     break;
                 case 0:
                     System.out.println("프로그램을 종료합니다.");
-                    System.out.println("선택하신 음료는 ");
-
-                    for (String s : selectList) {
-                        for (MenuItem menuItem : menuItems) {
-                            if (menuItem.getCoffee().equals(s)) {
-                                System.out.println(menuItem);
+                    // 선택된 음료가 있을 경우에만 출력
+                    if(!selectList.isEmpty()){
+                        System.out.println("선택하신 메뉴는 ");
+                        for (String s : selectList) {
+                            for (MenuItem menuItem : menuItems) {
+                                if (menuItem.getCoffee().equals(s)) {
+                                    System.out.println(menuItem);
+                                }
                             }
                         }
+                        System.out.println("입니다.");
+                    }else{
+                        System.out.println("선택하신 음료가 없습니다.");
                     }
-                    System.out.println("입니다.");
                     // 강제 종료하는 느낌이 강함. break 추천.
-                    System.exit(0);
+                    check = false;
                     break;
                 default:
                     System.out.println("잘못된 번호를 입력하셨습니다!");
--- src/lv3/Main.java
+++ src/lv3/Main.java
@@ -1,12 +1,24 @@
 package lv3;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
-        Kiosk kiosk = new Kiosk();
+        List<MenuItem> menuItems = new ArrayList<>();
+
+        Kiosk kiosk = new Kiosk(menuItems);
+        menuItems.add(new MenuItem("Espresso", 2500, "고온ㆍ고압하에서 곱게 간 커피 가루에 물을 가해, 30초 이내에 내린 커피."));
+        menuItems.add(new MenuItem("Americano", 3000, "에스프레소에 물을 넣어 연하게 마시는 커피."));
+        menuItems.add(new MenuItem("Caffé Latte", 3500, "에스프레소에 따뜻한 우유를 1:2 또는 1:3 정도의 비율로 섞은 커피."));
+        menuItems.add(new MenuItem("Caramel Macchiato", 4000, "에스프레소에 캐러멜 소스와 우유를 넣고 우유 거품만 살짝 올린 커피."));
+        menuItems.add(new MenuItem("Café Mocha", 4500, "에스프레소에 우유와 초콜릿을 넣고 휘핑크림을 올린 커피."));
 
         kiosk.start();
 
     }
+
+
 }
