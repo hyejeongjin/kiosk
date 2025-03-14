185628a553b46968f55a5029b9817e4049b1e391
hyejeongjin <hyejeongjin.dev@gmail.com>
2025-03-14 12:33:32 +0900

    :Fix: 사용하지 않는 getter 삭제

--- src/lv4/Menu.java
+++ src/lv4/Menu.java
@@ -21,11 +21,6 @@ public class Menu {
         return category;
     }
 
-    public List<MenuItem> getMenuItems() {
-        return menuItems;
-    }
-
-
     //메뉴를 순차적으로 보여주는 함수
     public void showMenuItems(){
         for (int i=0; i<menuItems.size(); i++) {
